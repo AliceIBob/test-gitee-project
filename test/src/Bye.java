677a3c1a99a45b856dac72700c40d15597be2237
wangzheng <1054215169@qq.com>
2023-07-13 16:12:01 +0800

    dinner

--- test/src/Bye.java
+++ test/src/Bye.java
@@ -1,43 +1,5 @@
 public class Bye {
     public static void main(String[] args) {
-        System.out.println("Bye");
-        System.out.println("Bye");
-        System.out.println("Bye");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
-        System.out.println("____________");
+        System.out.println("Bye,have dinner");
     }
 }
