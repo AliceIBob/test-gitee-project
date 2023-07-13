c5105bb07db85f5e19ac9cd254cdd6014bb98f95
wangzheng <1054215169@qq.com>
2023-07-13 15:33:02 +0800

    geng gai

--- test/src/Bye.java
+++ test/src/Bye.java
@@ -2,5 +2,6 @@ public class Bye {
     public static void main(String[] args) {
         System.out.println("Bye");
         System.out.println("Bye");
+        System.out.println("____________");
     }
 }
